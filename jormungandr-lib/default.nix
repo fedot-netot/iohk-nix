ec95f394c4a52c963ecf4e253899d2ef1f0b49fd
Samuel Leathers <samuel.leathers@iohk.io>
2020-06-21 21:39:08 -0400

    jormungandr: v0.9.0-rc2

--- jormungandr-lib/default.nix
+++ jormungandr-lib/default.nix
@@ -406,7 +406,7 @@ let
     };
 
     nightly = {
-      packages = packages.v0_9_0-rc1;
+      packages = packages.v0_9_0-rc2;
       genesisHash = "9409af111b04896c756c1cee3b7f9bae8b9ed1843c9e0a5f07d92ab9b62f6f78";
       genesisFile = ./genesis-nightly.yaml;
       registryUrl = "https://github.com/input-output-hk/testnet-stakepool-registry/archive/master.zip";
@@ -437,7 +437,7 @@ let
     };
 
     qa = {
-      packages = packages.master;
+      packages = packages.v0_9_0-rc2;
       genesisHash = "b5ada4577e10513240a5457490bd855c350b378fcd762d4354b443ba49b42dda";
       genesisFile = ./genesis-qa.yaml;
       registryUrl = "https://explorer.qa.jormungandr-testnet.iohkdev.io/stakepool-registry/registry.zip";
